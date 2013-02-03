6c648c4eb0df2f45026b6036424fea8f781bb9bf
jkroso <jkroso@gmail.com>
2013-02-04 00:14:45 +1300

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,61 @@
+# Buildfresh
+
+Automatically run a command and reload your browser when files change
+
+## Getting Started
+
+	$ npm install jkroso/buildfresh --save
+
+## Basic Usage
+
+The simplest possible case is `buildfresh` followed by whatever you normal build command is
+
+	$ buildfresh make
+
+I will monitor __all__ files within the current working and run `make` whenever one changes before sending a signal to liveReload to refresh the browser. Often this won't be good enough though since if the built file is within the CWD it will trigger another file change event and thereby start an infinite loop. To prevent this you can reduce the files being monitored in a number of ways. 
+
+### Selecting the top level directory
+
+	$ buildfresh -d src
+
+Will not look at anything above `$CWD/src/`. In a lot of cases this will be enough but if not you also have some filtering options. See below.
+
+### Filtering files
+
+	$ buildfresh -e node_modules\|public
+
+Will prevent files with either "node_modules" or "public" in their path from being watched. Its a good idea to exclude "node_modules" anyway since it could slow things down if their are a lot of them.
+
+	$ buildfresh -i app
+
+Will monitor only files with "app" somewhere in their path
+
+## Thanks 
+To Andrey Tarantsov for the awesome [liveReload](http://livereload.com/) tool chain and filewatching tools. This tool is just a thin interface on your work.
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2012 Andrey Tarantsov
+
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
---
+++ bin/cli.js
@@ -0,0 +1,117 @@
+#!/usr/bin/env node
+
+var program = require('commander')
+  , path = require('path')
+  , fs   = require('fs')
+  , exec = require('child_process').exec
+
+program.version(require('../package').version)
+	.usage('[options] <command> to run before reloading the browser')
+	.option('-d, --directory <path>', 'absolute or relative to the CWD', path.resolve)
+	.option('-e, --exclude <regexp>', 'a regexp to match against file paths', RegExp)
+	.option('-i, --include <regexp>', 'a regexp to match against file paths', RegExp)
+	.option('-v, --verbose', 'show whats going on')
+	.parse(process.argv)
+
+// the env has to be set before loading the submodules
+// which use debug statements
+if (program.verbose) process.env.DEBUG = '*'
+
+var fsmonitor = require('fsmonitor')
+  , LiveReloadServer = require('livereload-server')
+  , debug = require('debug')('buildfresh')
+
+// default to CWD
+if (!program.directory) program.directory = process.cwd()
+
+// Join the leftover args since they make up the command
+var command = program.args.join(' ')
+
+// Create the filter functions if they are required
+var filter
+if (program.include || program.exclude) {
+	debug('Including files matching: %s', program.include || 'all')
+	debug('Excluding files matching: %s', program.exclude || 'null')
+	filter = {
+		matches: program.include
+			? function (path) {
+				return !!path.match(program.include)
+			}
+			: function () {return true},
+		excludes: program.exclude 
+			? function (path) {
+				return !!path.match(program.exclude)
+			}
+			: function () {return false}
+	}
+}
+
+var server = new LiveReloadServer({
+	// identifies your app
+	id: "component",
+	name: "development",
+	version: "1.0",
+	// protocols specifies the versions of subprotocols you support
+	protocols: {
+		monitoring: 7,
+		saving: 1
+	} 
+});
+
+server.on('connected', function(connection) {
+	debug("Client connected (%s)", connection.id)
+});
+
+server.on('disconnected', function(connection) {
+	debug("Client disconnected (%s)", connection.id)
+})
+
+server.on('command', function(connection, message) {
+	debug("Received command %s: %j", message.command, message)
+})
+
+server.on('error', function(err, connection) {
+	console.warn("Error (%s): %s", connection.id, err.message)
+})
+
+server.on('livereload.js', function(request, response) {
+	debug("Serving livereload.js.")
+	fs.readFile(require.resolve('./livereload'), 'utf8', function(err, data) {
+		if (err) throw err
+		response.writeHead(200, {'Content-Length': data.length, 'Content-Type': 'text/javascript'})
+		response.end(data)
+	})
+})
+
+server.on('httprequest', function(url, request, response) {
+	response.writeHead(404)
+	response.end()
+})
+
+server.listen(function(err) {
+	if (err) console.warn("Listening failed: %s", err.message)
+	else console.warn("Listening on port %d.", server.port)
+})
+
+/*!
+ * Watch for changes
+ */
+console.warn('Watching directory: %s', program.directory)
+
+fsmonitor.watch(program.directory, filter, function(change) {
+	debug("Change detected:\n" + change)
+
+	debug('Running command: "%s"', command)
+	exec(command, {stdio: 'inherit'}, function (err) {
+		if (err) throw err
+		var file = change.modifiedFiles[0] || ''
+		// Refresh the browser
+		connections.forEach(function (con) {
+			con.send({command:'reload', path:file, liveCSS:true})
+		})
+	})
+	var connections = Object.keys(server.connections).map(function (id) {
+		return server.connections[id]
+	})
+	if (!connections.length) console.warn('No browsers connected')
+})
